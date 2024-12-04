08a1e0e63c0c0c8ed7d7aee7bfb29f3237a99240
logg1s <lrng159@gmail.com>
2024-12-04 21:11:36 +0700

    add async storage

--- components/ButtonAddTodo/ButtonAddTodo.tsx
+++ components/ButtonAddTodo/ButtonAddTodo.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { s } from "./ButtonAddTodo.style";
 
 export function ButtonAddTodo({
