7165b0dfd2714cb37ec1c0674e766eb01706453f
logg1s <lrng159@gmail.com>
2024-12-04 20:56:32 +0700

    add button add todo component

---
+++ components/ButtonAddTodo/ButtonAddTodo.style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  btnAdd: {
+    backgroundColor: "#C2DAFF",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 100,
+    height: 40,
+    borderRadius: 8,
+  },
+  text: {
+    color: "#2F76E5",
+    fontSize: 14,
+  },
+});
---
+++ components/ButtonAddTodo/ButtonAddTodo.tsx
@@ -0,0 +1,14 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { s } from "./ButtonAddTodo.style";
+
+export function ButtonAddTodo({
+  onPressAddTodo,
+}: {
+  readonly onPressAddTodo: () => void;
+}) {
+  return (
+    <TouchableOpacity style={s.btnAdd} onPress={onPressAddTodo}>
+      <Text style={s.text}>+ New Todo</Text>
+    </TouchableOpacity>
+  );
+}
